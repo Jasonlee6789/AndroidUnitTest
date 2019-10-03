8ddc8105fb73d1dccbcb5aeb6b2f452093b41787
Jasonlee6789 <jeffersonlee@163.com>
2019-10-03 13:26:41 +1300

    I create a Expression

---
+++ app/src/main/java/com/example/unittest_espression/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.unittest_espression;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnGetMessage;
+    private EditText et_meaasgeToCopay;
+    private TextView tv_copiedMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        et_meaasgeToCopay = findViewById(R.id.editText_messageToCopy);
+        tv_copiedMessage = findViewById(R.id.textView_copiedMeaasge);
+
+    }
+
+    public void getMessage(View view){
+        String message = et_meaasgeToCopay.getText().toString();
+        tv_copiedMessage.setText(message);
+        tv_copiedMessage.setTextColor(Color.RED);
+    }
+}
