288e2db6d26747b749c3dc1cc8631397832c2966
Jasonlee6789 <jeffersonlee@163.com>
2019-10-03 14:42:04 +1300

    I completed the test for the activity2

--- app/src/androidTest/java/com/example/unittest_espression/MainActivityTest.java
+++ app/src/androidTest/java/com/example/unittest_espression/MainActivityTest.java
@@ -24,8 +24,8 @@ public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
-//this line is for testing the access to the second activity
-    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(Activity2.class.getName(),null,false);
+    //this line is for testing the access to the second activity
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(Activity2.class.getName(), null, false);
 
     // create a instance of the target class
     MainActivity mainActivity;
@@ -38,22 +38,21 @@ public class MainActivityTest {
         mainActivity = activityTestRule.getActivity();
         messageToDisply = "This is a test message";
     }
-// this @Test annotation is commented out so it won't be expected for the second test
+    // this @Test annotation is commented out so it won't be expected for the second test
     @Test // import  library  from second
-    public void testUserInputScenario(){
+    public void testUserInputScenario() {
         onView(withId(R.id.editText_messageToCopy)).perform(typeText(messageToDisply));
         closeSoftKeyboard();
         onView(withId(R.id.btn_getMessage)).perform(click());
         onView(withId(R.id.textView_copiedMeaasge)).check(matches(withText(messageToDisply)));
     }
     // this is for testing the access to the second activity
-
     @Test
-    public void testLaunchActivity2(){
+    public void testLaunchActivity2() {
         assertNotNull(mainActivity.findViewById(R.id.btn_openActivity2));
         onView(withId(R.id.btn_openActivity2)).perform(click());
 
-        Activity activity2 = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        Activity activity2 = getInstrumentation().waitForMonitorWithTimeout(monitor, 5000);
         assertNotNull(activity2);
     }
 
